215511148cb276a789d98c173cad8ec01f9ac774
[Raúl Gómez Illán] <[rgillan@uoc.edu]>
2026-05-25 14:08:06 +0200

    fix: Revisión botones y pestañas
    Se dota a todos los botones del mismo aspecto y feedback hacia el usuario, pestaás vehículos y reglas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
